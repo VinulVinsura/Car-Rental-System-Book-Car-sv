851ad4f0df2ac832fed9a03d8883eb2f29480a6b
Vinul Vinsura <vinulviksura16@gmail.com>
2024-06-18 23:53:59 +0530

    first commit

---
+++ Car-Rental-System-Book-Car-sv/src/main/java/com/example/carrentalsystembookcarsv/CarRentalSystemBookCarSvApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrentalsystembookcarsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSystemBookCarSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalSystemBookCarSvApplication.class, args);
+	}
+
+}
